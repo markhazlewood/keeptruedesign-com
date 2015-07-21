52b95aa1c16c3c9ed7f239d2d2a194ed704e6779
mark.h <mark@mhazlewood.org>
2015-07-20 21:31:43 -0400

    Added initial BrowserSync config (still needs work to serve up a PHP site), and updated index content wording.

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
 	if (isset($_POST['submit']))
 	{
 		$error = "";
-		
+
 		if (!empty($_POST['name']))
 		{
 			$name = $_POST['name'];
@@ -16,22 +16,22 @@
 		else
 		{
 			$error .= "<li>Your name</li>";
-		}		
-		
+		}
+
 		if (!empty($_POST['email']))
 		{
 			$email = $_POST['email'];
-			
+
 			if (!preg_match("/^[_a-z0-9]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email))
-			{ 
+			{
 				$error .= '<li>A <strong>valid</strong> email address ("' . $email . '" is not valid)</li>';
 			}
-		} 
-		else 
+		}
+		else
 		{
 			$error .= "<li>An email address where we can contact you</li>";
 		}
-		
+
 		if (!empty($_POST['message']))
 		{
 			$message = $_POST['message'];
@@ -40,7 +40,7 @@
 		{
 			$error .= "<li>A message describing the kind of work you are interested in</li>";
 		}
-		
+
 		if (($_POST['code']) == $_SESSION['code'])
 		{
 			$code = $_POST['code'];
@@ -49,18 +49,18 @@
 		{
 			$error .= "<li>A valid code so we know you are a person</li>";
 		}
-		
+
 		if (empty($error))
 		{
 			$from = 'From: ' . $name . ' <' . $email . '>';
 			$to = 'info@keeptruedesign.com';
 			$subject = 'New message from website contact form';
 			$success = "<p><strong>Thank you! Your message has been sent!</strong></p>";
-			
+
 			mail($to, $subject, $message, $from);
 		}
 	}
-		
+
 ?>
 
 
@@ -91,7 +91,7 @@
 <body>
 <?php include_once("analyticstracking.php") ?>
   <div class="grid-container" style="">
-	
+
     <div class="grid-100 top-bar">
 		<section class="main-navigation">
 			<ul>
@@ -101,42 +101,42 @@
 			</ul>
 		</section>
     </div>
-	
+
 	<div class="content-container">
-	
-		<div class="grid-100 header-container">			
+
+		<div class="grid-100 header-container">
 			<img src="./assets/KT_logo.png" width="138" height="138" style="padding-left: 80px; float: left; " />
 			<span style="float: left; margin-left: 20px; ">
 				<h1>
 					Keep True Design
 				</h1>
-			</span>				
+			</span>
 			<span style="float: left; margin-left: 120px; margin-top: -20px; ">
 				<h2>Building Unique and Engaging Experiences</h2>
 			</span>
 		</div>
-		
+
 		<div class="grid-100" style="text-align: right; margin-top: -24px; margin-left: -10px; ">
 			<a href="http://www.facebook.com/KeepTrueDesign" target="_blank"><img src="./assets/facebook_icon.png" width="48px" height="48px" /></a>
 			<a href="http://twitter.com/keeptruedesign" target="_blank"><img src="./assets/twitter_icon.png" width="48px" height="48px" /></a>
 		</div>
-		
+
 		<div class="grid-100 body-container" style="margin-top: -30px; ">
 			<h1>Get in touch</h1>
 			<p>
 				You can call directly, email, or just fill out the form below to get in touch.
 				We'll provide a free estimate on your project, and we can typically respond within
 				one business day.
-			</p>			
+			</p>
 		</div>
-		
+
 		<hr />
-		
+
 		<div class="grid-100 body-container">
 			<div class="grid-66" style="border-right: 1px solid gray; ">
-				
+
 				<div id="contact-form">
-				
+
 					<?php
 						if (!empty($error))
 						{
@@ -151,33 +151,33 @@
 							echo $success;
 						}
 					?>
-				
+
 					<form action="contact.php" method="post">
-      
+
 						<label>Name:</label>
 						<input type="text" name="name" title="How would you like to be known?" value="<?php if ($_POST['name']) { echo $_POST['name']; } ?>" />
-				  
+
 						<label>Email:</label>
 						<input type="text" name="email" title="An address where we can get back to you" value="<?php if ($_POST['email']) { echo $_POST['email']; } ?>" />
-						
+
 						<label>Message:</label><br />
 						<textarea name="message" rows="20" cols="20" title="Let us know how can we help"><?php if ($_POST['message']) { echo $_POST['message']; } ?></textarea>
-						
+
 						<label><img src="captcha.php" /></label>
 						<input type="text" name="code" title="Copy this code so we know you are a person">
 						<br />
-						
+
 						<input type="submit" class="submit" name="submit" value="Send message" />
-						
+
 					  </form>
 				</div>
-				
+
 			</div>
 			<div class="grid-33" style="padding-left: 20px; ">
 				<img src="./assets/phone.png" width="48px" height="48px" style="float: left; padding-right: 10px; " />
 				<p>
 					<strong>Phone</strong> <br />
-					<span style="font-size: 0.75em; ">						
+					<span style="font-size: 0.75em; ">
 						<script type="text/javascript" language="javascript">
 						<!--
 						// Email obfuscator script 2.1 by Tim Williams, University of Arizona
@@ -193,7 +193,7 @@
 							  ltr = coded.charAt(i)
 							  link += (ltr)
 							}
-							else {     
+							else {
 							  ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
 							  link += (key.charAt(ltr))
 							}
@@ -204,9 +204,9 @@
 						</script><noscript>Sorry, you need Javascript on see my phone number.</noscript>
 					</span>
 				</p>
-				
+
 				<br />
-				
+
 				<img src="./assets/email.png" width="48px" height="48px" style="float: left; padding-right: 10px; " />
 				<p>
 					<strong>Email</strong> <br />
@@ -226,7 +226,7 @@
 						  ltr = coded.charAt(i)
 						  link += (ltr)
 						}
-						else {     
+						else {
 						  ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
 						  link += (key.charAt(ltr))
 						}
@@ -240,12 +240,12 @@
 				</p>
 			</div>
 		</div>
-		
+
 		<hr />
 		<hr />
-	
+
 	</div>
-	
+
 	<div class="grid-100 bottom-bar">
 		<section class="footer">
 			<p style="float: left; ">
@@ -258,15 +258,15 @@
 			</p>
 		</section>
 	</div>
-    
+
   </div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
   <!--<script src="./js/responsive.js"></script>-->
-  
+
   <script>
-	$(function() 
-	{	
+	$(function()
+	{
 		$(document).tooltip(
 		{
 			position:
@@ -274,21 +274,21 @@
 				my: "left+20 center",
 				at: "right center"
 			},
-			
+
 			show:
 			{
 				effect: "fadeIn",
 				duration: 100
 			},
-			
+
 			hide:
 			{
 				effect: "fadeOut",
 				duration: 100
-			}			
+			}
 		});
 	});
 </script>
-  
+
 </body>
 </html>
